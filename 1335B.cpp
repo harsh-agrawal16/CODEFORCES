c557bc6071f4ad855246f29a4863c30795cfc1e0
harsh-agrawal16 <agrwl.harsh@gmail.com>
2020-04-15 13:00:23 +0530

    Construct The String

--- 1335B.cpp
+++ 1335B.cpp
@@ -10,24 +10,43 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
-        ll n,x;
-        cin>>n>>x;
+        ll n,a,b;
+        cin>>n>>a>>b;
 
-        ll arr[n];
-        for(ll i=0;i<n;i++){
-            cin>>arr[i];
+        vector<ll> a1(26,-1),a2(26,-1);
+        char x = 'a';
+        string ans = "";
+        for(ll i=0;i<b;i++){
+            ans+=x;
+            a1[x-'a']=1;
+            x++;
         }
-        sort(arr,arr+n);
-
-        ll ans=0;
-        ll sum=0;
-        for(ll i=n-1;i>=0;i--){
-            sum+=arr[i];
-            if((sum/(n-i)>=x)) ans++;
+        
+        x--;
+        while(b<a){
+            ans+=x;
+            a1[x-'a']++;
+            b++;
+        }
+        a2 = a1;
+        
+        ll k=0;
+        a2[ans[k]-'a']--;
+        for(ll j=a;j<n;j++){
+            for(ll i=0;i<26;i++){
+                if(a1[i]==-1) continue;
+                if(a2[i]<a1[i]){
+                    ans+=ans[k];
+                    k++;
+                    a2[i]=a1[i];
+                    break;
+                }
+            }
+            a2[ans[k]-'a']--;
         }
 
         cout<<ans<<"\n";
-
+        
     }
 
 }
