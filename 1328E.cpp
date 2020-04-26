96c5f0cacde3962ffa49b3c198d4ffde241585d0
harsh-agrawal16 <agrwl.harsh@gmail.com>
2020-04-26 17:09:27 +0530

    Done ,Got to learn a beautiful concept about trees, how to determine whether or not a node is a ancestor of another in just O(V) and many more

--- 1328E.cpp
+++ 1328E.cpp
@@ -1,4 +1,3 @@
-
 /**
         *AUTHOR:Harsh Agrawal*
         *Birla Institute of Technology,Mesra*    
@@ -6,64 +5,74 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef int ll;
 typedef unsigned long long ull;
 const int N=2e5+1;
-#define forn(i,e) for(ll i=0;i<e;i++)
-#define forni(i,x,e) for(ll i=x;i<e;i++)
+#define for0(i,e) for(int i=0;i<e;i++)
+#define forx(i,x,e) for(int i=x;i<e;i++)
 #define ln "\n"
 #define in(a) cin>>a
 #define out(a) cout>>a;
+#define pb push_back
+#define vll vector<ll> 
+#define vvll vector<vll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define qll queue<ll>
+#define fastIO  ios_base::sync_with_stdio(0);\
+                cin.tie(NULL);\
+                cout.tie(NULL)
+vi pa;
+vvi adj;
+vi d;
+vi tin,tout;
+int T;
 
-vector<vector<ll>> adList(N);
-
-bool dfs(ll u, ll prev, vector<bool> q, ll count){
-    if(q[u])count--;
-    if(count==0) return true;
-
-    ll temp = count;
-    bool te = false;
-    for(auto v: adList[u]){
-        if(adList[u].size()>=2){for(auto x: adList[u]) if(x!=prev && x!=v) if(q[x]) count--;}
-        if(count == 0 ) return true;
-        if(v!=prev){
-            te = te || dfs(v,u,q,count);
-        } 
-        if(te) return true;
-        count = temp;
+void dfs(int u, int prev, int dep){
+    d[u] = dep;
+    pa[u] = prev;
+    tin[u] = T++; //entry time.
+    for(auto edge : adj[u]){
+        if(edge != prev) dfs(edge,u,dep+1);
     }
+    tout[u] = T++; //exit time.
+}
 
+bool isAncestor(int u, int v){
+    if(tin[u]<=tin[v] && tout[u]>=tout[v]) return true;
     return false;
 }
 
 int main(){
-    ll n,m;cin>>n>>m;
-    
-    for(ll i=0;i<n-1;i++){
-        ll x,y;cin>>x;cin>>y;
-        adList[x].push_back(y);
-        adList[y].push_back(x);
+    fastIO;
+    int n,m;
+    cin>>n>>m;
+    adj = vvi(n+1);
+    T=0;
+    tin = vi(n+1); tout = vi(n+1);
+    pa = vi(n+1); d = vi(n+1);
+    for0(i,n-1){
+        int x,y;cin>>x>>y;
+        adj[x].pb(y);
+        adj[y].pb(x);
     }
 
-    //vector<bool> q(m+1,false);
-    for(ll i=0;i<m;i++){
-        vector<bool> q(n+1,false);
-        ll k;
-        cin>>k;
-        ll count =0;
-        forn(i,k){
-            ll x;cin>>x;
-            q[x] = true;
-            count++; //since all are different.
+    dfs(1,-1,0);
+    while(m--){
+        int k;cin>>k;
+        vi q(k); for0(i,k) cin>>q[i];
+        int u = q[0];
+        forx(i,1,k) if(d[u]<d[q[i]]) u = q[i];
+        for0(i,k){
+            if(q[i]!=u && pa[q[i]]!=-1) q[i] = pa[q[i]];
         }
-        
-        if(q[1]) count--;
-        if(count==0){cout<<"YES"<<ln; continue;}
-        if(dfs(1,-1,q,count)) cout<<"YES"<<ln; 
-        else cout<<"NO"<<ln;
+        bool done = true;
+        for0(i,k){
+            if(q[i]!=u && q[i]!=1) done&=isAncestor(q[i],u);
+        }
+        if(done) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
 
     
 }
-
- 
